c84ce5b19ec1ab54d7797cee9e425df957e992c0
parkajeong <milk5253@gmail.com>
2025-06-12 13:43:58 +0900

    commit1 뼈대

---
+++ TimeTable.java
@@ -0,0 +1,29 @@
+public class TimeTable {
+    String classnumber;
+    String time;
+    boolean classState;
+    
+
+    TimeTable(String classnumber, String time, boolean classState){
+        this.classnumber = classnumber;
+        this.time = time;
+        this.classState = classState;
+    }
+
+    public void ShowTimeTable(){
+        System.out.println("==========Time Table==========");
+        for(int i=0;i<4;i++){
+            
+        }
+
+    }
+
+    public void AddClass(){
+
+    }
+
+    public void RemoveClass(){
+
+    }
+
+}
