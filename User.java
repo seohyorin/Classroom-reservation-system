1532aeb9f2b3738459a9efa317b69c4cea4b62cf
simpleb05 <effet3308@naver.com>
2025-06-12 16:07:38 +0900

    사용자 클래스에서 학번이름 입력 및 출력 추가한 코드

--- User.java
+++ User.java
@@ -6,18 +6,24 @@ public class User extends Timetable {
         super();
     }
 
-    public void requestClass(String classroom) {
+    public void requestClass(String classroom, String idname) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("학번이름을 입력하세요.");
+        String id = scanner.next();
         System.out.println("예약할 강의실의 번호를 입력하세요.");
         int calssroom = scanner.nextInt();
+        System.out.println("학번이름: " + idname);
         System.out.println("신청한 강의실: " + classroom);
     }
 
-    public void cancelClass(String classroom) {
+    public void cancelClass(String classroom, String idname) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("취소할할 강의실의 번호를 입력하세요.");
+        System.out.println("학번이름을 입력하세요.");
+        String id = scanner.next();
+        System.out.println("취소할 강의실의 번호를 입력하세요.");
         int calssroom = scanner.nextInt();
-        System.out.println("취소한 강의실: " + classroom);
+        System.out.println("학번이름: " + idname);
+        System.out.println("한 강의실: " + classroom);
     }
 
 }
