fed7172464dee9ea3369e82a311c936b3675b530
parkajeong <milk5253@gmail.com>
2025-06-12 14:08:46 +0900

    화면 출력 및 예약 검사사

--- TimeTable.java
+++ TimeTable.java
@@ -7,23 +7,22 @@ public class TimeTable {
     TimeTable(String classnumber, String time, boolean classState){
         this.classnumber = classnumber;
         this.time = time;
-        this.classState = classState;
+        this.classState = false;
+    }
+
+    public String CheckState(){
+        if(classState == false){
+            return "예약 가능";
+        }else{
+            return "예약 불가능";
+        }
     }
 
     public void ShowTimeTable(){
-        System.out.println("==========Time Table==========");
+        System.out.println("===============Time Table===============");
         for(int i=0;i<4;i++){
-            
+            System.out.println("[호실: "+classnumber+ " | 시간: " +time+ " | 예약상태:"+ CheckState()+"]");
         }
 
     }
-
-    public void AddClass(){
-
-    }
-
-    public void RemoveClass(){
-
-    }
-
 }
