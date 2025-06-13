def84c97a5b9de427a2010049c14a639b032de32
simpleb05 <effet3308@naver.com>
2025-06-13 16:11:15 +0900

    Update User.java

--- User.java
+++ User.java
@@ -1,23 +1,46 @@
 import java.util.Scanner;
 
 public class User extends Timetable {
+    private BookingManager bookingManager;
 
-    public User() {
+    public User(BookingManager bookingManager) {
         super();
+        this.bookingManager = bookingManager;
     }
 
-    public void requestClass(String classroom) {
+    public void requestClass() {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("학번이름을 입력하세요.");
+        String enteredStudentIdName = scanner.next();
         System.out.println("예약할 강의실의 번호를 입력하세요.");
-        int calssroom = scanner.nextInt();
-        System.out.println("신청한 강의실: " + classroom);
+        String enteredClassroomNumber = scanner.next();
+
+        bookingManager.addBookingRequest(enteredStudentIdName, enteredClassroomNumber);
     }
 
-    public void cancelClass(String classroom) {
+    public void cancelClass() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("취소할할 강의실의 번호를 입력하세요.");
-        int calssroom = scanner.nextInt();
-        System.out.println("취소한 강의실: " + classroom);
+        System.out.println("학번이름을 입력하세요.");
+        String enteredStudentIdName = scanner.next();
+        System.out.println("취소할 강의실의 번호를 입력하세요.");
+        String enteredClassroomNumber = scanner.next();
+        
+        String result = bookingManager.cancelBooking(enteredStudentIdName, enteredClassroomNumber);
+        System.out.println(result);
     }
 
+    public void checkBookingStatus() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("예약 상태를 확인할 학번이름을 입력하세요.");
+        String studentIdName = scanner.next();
+        System.out.println("예약 상태를 확인할 강의실 번호를 입력하세요.");
+        String classroomNumber = scanner.next();
+
+        String status = bookingManager.getBookingStatus(studentIdName, classroomNumber);
+        if ("찾을 수 없음".equals(status)) {
+            System.out.println("해당하는 예약이 없습니다.");
+        } else {
+            System.out.println("예약 상태: " + status);
+        }
+    }
 }
